fe2525daf50ce798a1096ca4c7c34e7c1e3504d3
Divyanshu Singhania <divyanshu6927@gmail.com>
2024-09-05 13:57:01 +0530

    Added comment in code for auto updation of servers

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"Hello": "World"}   # FastAPI now Auto updates 
+                                # hence we dont need to write commands 
+                                # or any thing for it niether we need to restart the servers
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
